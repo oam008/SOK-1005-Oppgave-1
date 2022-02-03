a17fc501952be76d77ba5eb4e7c4a9535dff3ea2
oam008 <98551757+oam008@users.noreply.github.com>
2022-02-03 13:54:53 +0100

    Add files via upload

---
+++ Oppgave 1a oppdatert.R
@@ -0,0 +1,29 @@
+mutatinstall.packages("readr")
+install.packages("zoo")
+install.packages("lubridate")
+library(readr)
+library(ggplot2)
+library(scales)
+library(zoo)
+library(lubridate)
+library(dplyr)
+
+lower_troposphere <- read_table("https://www.nsstc.uah.edu/data/msu/v6.0/tlt/uahncdc_lt_6.0.txt")
+
+
+l_t <- head(lower_troposphere, - 12)
+
+l_t <-
+  l_t %>%
+  mutate(dato = ymd(paste(Year, Mo, 1, sep = "-"))) 
+
+l_t <-
+  l_t %>%
+  select(dato, Globe) %>%
+  mutate(cma = rollmean(Globe, 13, fill = NA, align = "center"))
+
+
+
+GlobalTemps <- ggplot(l_t, aes(x = dato, y = as.numeric(Globe))) + geom_point(size = 0.5, col = "blue") + geom_line(group = 1, col = "blue") + geom_line(aes(y = cma), col = "red", lwd = 1.2) + scale_y_continuous(breaks = seq(-1,1, by = 0.1)) + ggtitle("UAH Satellite-Based Temperature of the Global Lower Atmosphere") + xlab("Year") + ylab("T Departure from '91-'20 Avg (deg. C)")
+GlobalTemps
+
